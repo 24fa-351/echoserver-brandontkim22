800a928aaadf8323245964e0085f3a6f3d07d4cf
bkim <112056836+brandontkim22@users.noreply.github.com>
2024-11-06 17:11:20 -0800

    updated to multithread

--- echo.c
+++ echo.c
@@ -5,22 +5,28 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define LISTEN_BACKLOG 5
 
-void handleConnection(int a_client)
+void* handleConnection(void* a_client_ptr)
 {
-    char buffer[1024];
+    int a_client = *(int*)a_client_ptr;
+    free(a_client_ptr);
     
     while (1) {
+        char buffer[1024];
         int bytes_read = read(a_client, buffer, sizeof(buffer));
         if (bytes_read <= 0) {
+            printf("Connection closed on socket_fd %d\n", a_client);
             break;
         }
-        printf("Received %s\n", buffer);
+        printf("Received from socket_fd %d: %s\n", a_client, buffer);
         write(a_client, buffer, bytes_read);
         bytes_read = read(a_client, buffer, sizeof(buffer));
     }
+
+    return NULL;
 }
 
 int main(int argc, char* argv[]) {
@@ -43,6 +49,10 @@ int main(int argc, char* argv[]) {
 
     returnval = bind(
         socket_fd, (struct sockaddr*)&socket_address, sizeof(socket_address));
+    if (returnval == -1) {
+        perror("bind");
+        return 1;
+    }
 
     returnval = listen(socket_fd, LISTEN_BACKLOG);
 
@@ -52,8 +62,15 @@ int main(int argc, char* argv[]) {
         socklen_t client_address_len = sizeof(client_address);
         int client_fd = accept(
             socket_fd, (struct sockaddr*)&client_address, &client_address_len);
-        handleConnection(client_fd);
-        close(client_fd);
+        if (client_fd == -1) {
+            perror("accept");
+            return 1;
+        }
+        int* client_fd_ptr = (int*)malloc(sizeof(int));
+        *client_fd_ptr = client_fd;
+
+        pthread_t thread;
+        pthread_create(&thread, NULL, handleConnection, (void*)client_fd_ptr);
     }
    
     return 0;
